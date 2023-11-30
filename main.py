89484fbe974cc4bce8ad17f771e533ac815d233c
Dhjali <barbara.vanni@laplateforme.io>
2023-11-30 15:38:54 +0100

    aller plus loin

--- main.py
+++ main.py
@@ -6,16 +6,87 @@ from forex_python.converter import *
 # Quel montant ils veulent convertir 
 
 c = CurrencyRates()
+history= []
 
-def devise_actuelle():
-    pass
+
+def convert_currency(amount, from_currency, to_currency):
+        try:
+            rate = c.get_rate(from_currency, to_currency)
+            converted_amount = amount * rate
+            conversion_result = {
+                'amount': amount,
+                'from_currency': from_currency,
+                'to_currency': to_currency,
+                'converted_amount': converted_amount
+            }
+            history.append(conversion_result)
+            return converted_amount
+        except:
+            print("La conversion est impossible. Veuillez vérifier les devises fournies.")
+            return None
+
+def add_currency(currency, rate):
+    c.get_rate(currency, rate)
+
+def add_favorite_currency(currency):
+
+    favorites = get_favorite_currencies()
+    favorites.append(currency)
+    with open('favorite_currencies.json', 'w') as file:
+        json.dump(favorites, file)
+
+def get_favorite_currencies():
+    try:
+        with open('favorite_currencies.json', 'r') as file:
+            favorites = json.load(file)
+        return favorites
+    except FileNotFoundError:
+        return []
+
+def print_conversion_history():
+    print("Historique des conversions:")
+    for entry in history:
+        print(f"{entry['amount']} {entry['from_currency']} => {entry['converted_amount']} {entry['to_currency']}")
 
 def main():
     while True:
         print("Voici la liste des différentes devises disponibles:","\n" *2 ,*c.get_rates(''),"\n")
         
-        devise_actuel = str(input("Entrez votre devise actuelle: "))
-        devise_voulue = str(input("Entrez la devise que vous voulez: "))
-        amount = float(input("Quel montant voulez-vous convertir ? : "))
-        break
+        print("\nMenu:")
+        print("1. Convertir une somme")
+        print("2. Ajouter une devise")
+        print("3. Ajouter une devise préférée")
+        print("4. Afficher l'historique des conversions")
+        print("5. Quitter")
+
+        choice = input("Choisissez une option (1-5): ")
+
+        if choice == '1':
+            amount = float(input("Entrez le montant à convertir : "))
+            from_currency = input("Entrez la devise d'origine: ")
+            to_currency = input("Entrez la devise de destination: ")
+            converted_amount = convert_currency(amount, from_currency, to_currency)
+            if converted_amount is not None:
+                print(f"{amount} {from_currency} équivaut à {converted_amount} {to_currency}")
+
+        elif choice == '2':
+            currency = input("Entrez le code de la nouvelle devise : ")
+            rate = float(input(f"Entrez le taux de conversion de votre devise {currency} : "))
+            add_currency(currency, rate)
+            print(f"{currency} a été ajouté avec succès.")
+
+        elif choice == '3':
+            currency = input("Entrez le code de la devise préférée à ajouter : ")
+            add_favorite_currency(currency)
+            print(f"{currency} a été ajouté aux devises préférées.")
+
+        elif choice == '4':
+            print_conversion_history()
+
+        elif choice == '5':
+            print("Au revoir!")
+            break
+
+        else:
+            print("Option invalide. Veuillez choisir une option valide.")
 main()

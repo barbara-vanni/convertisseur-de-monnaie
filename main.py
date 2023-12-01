fd3e769172e2451fb9f9d821633002de8ae2c77b
Dhjali <barbara.vanni@laplateforme.io>
2023-12-01 11:47:28 +0100

    add new currency

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from forex_python.converter import *
+import json
 
 # demander quelle devise ? # montrer liste des différentes devises
 # montrer liste des autres devises possible ? (taux de change ou pas)
@@ -7,27 +8,47 @@ from forex_python.converter import *
 
 c = CurrencyRates()
 history= []
-favorite_currencies = []
 
 
 def convert_currency(amount, from_currency, to_currency):
-        try:
-            rate = c.get_rate(from_currency, to_currency)
-            converted_amount = amount * rate
-            conversion_result = {
-                'amount': amount,
-                'from_currency': from_currency,
-                'to_currency': to_currency,
-                'converted_amount': converted_amount
-            }
-            history.append(conversion_result)
-            return converted_amount
-        except:
-            print("La conversion est impossible. Veuillez vérifier les devises fournies.")
-            return None
+    try:
+        if from_currency in get_favorite_currencies():
+            print(f"Utilisation de la devise préférée: {from_currency}")
+        else:
+            print(f"La devise {from_currency} n'est pas une devise préférée.")
+
+        rate = c.get_rate(from_currency, to_currency)
+        converted_amount = amount * rate
+        conversion_result = {
+            'amount': amount,
+            'from_currency': from_currency,
+            'to_currency': to_currency,
+            'converted_amount': converted_amount
+        }
+        history.append(conversion_result)
+        return converted_amount
+    except:
+        print("La conversion est impossible. Veuillez vérifier les devises fournies.")
+        return None
+
 
 def add_currency(currency, rate):
-    c.get_rate(currency, rate)
+    new_currency = {'currency': currency, 'rate': rate}
+    
+    try:
+        with open('new_currencies.json', 'r') as file:
+            currencies = json.load(file)
+    except FileNotFoundError:
+        currencies = []
+
+    currencies.append(new_currency)
+
+    with open('new_currencies.json', 'w') as file:
+        json.dump(currencies, file)
+
+    return currencies
+
+    
 
 
 def add_favorite_currency(currency):
@@ -65,21 +86,42 @@ def main():
         print("5. Afficher l'historique des conversions")
         print("6. Quitter")
 
-        choice = input("Choisissez une option (1-5): ")
+        choice = input("Choisissez une option (1-6): ")
 
         if choice == '1':
             amount = float(input("Entrez le montant à convertir : "))
             from_currency = input("Entrez la devise d'origine: ")
-            to_currency = input("Entrez la devise de destination: ")
+
+            use_favorite = input("Voulez-vous utiliser une devise préférée ? (Oui/Non): ").lower()
+            if use_favorite == 'oui':
+                favorite_currencies_list = get_favorite_currencies()
+                if favorite_currencies_list:
+                    print("Devises préférées disponibles:", favorite_currencies_list)
+                    to_currency = input("Choisissez la devise de destination parmi les devises préférées : ")
+                else:
+                    print("Aucune devise préférée disponible. Veuillez en ajouter d'abord.")
+                    continue
+            else:
+                to_currency = input("Entrez la devise de destination: ")
+
             converted_amount = convert_currency(amount, from_currency, to_currency)
             if converted_amount is not None:
                 print(f"{amount} {from_currency} équivaut à {converted_amount} {to_currency}")
+
 
         elif choice == '2':
             currency = input("Entrez le code de la nouvelle devise : ")
             rate = float(input(f"Entrez le taux de conversion de votre devise {currency} : "))
             add_currency(currency, rate)
             print(f"{currency} a été ajouté avec succès.")
+            new_favorite = input("Voulez-vous ajouter la nouvelle devise à vos devises préférées ?(Oui/Non)").lower() 
+            if new_favorite == 'oui':
+                add_favorite_currency(add_currency)
+                print(f"{currency} a été ajouté à vos favories.")
+                # marche pas :(
+
+        
+
 
         elif choice == '3':
             currency = input("Entrez le code de la devise préférée à ajouter : ")
